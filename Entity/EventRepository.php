a6183a623865872fbfb02f4317ccaedce16c54d8
Christian <csoronellas@emagister.com>
2012-04-30 20:48:32 +0200

    Test

--- Entity/EventRepository.php
+++ Entity/EventRepository.php
@@ -10,10 +10,22 @@ class EventRepository extends AbstractRepository
      * Gets an event by its ID
      *
      * @param int $eventId
-     * @return stdClass
+     * @return object
      */
     public function findEvent($eventId)
     {
-        return $this->getClient()->event_get(array('id' => $eventId));
+        $response = $this->getClient()->event_get(array('id' => $eventId));
+        $body = $response->getBody();
+        return $this->map($body->read($body->getContentLength()));
+    }
+
+    /**
+     * Maps an entity from an XML representation to an object representation
+     *
+     * @param string $entity
+     * @return object
+     */
+    public function map($entity) {
+
     }
 }
