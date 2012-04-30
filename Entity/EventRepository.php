9d93fd21078a6ae93612575cc832069541d38120
Christian <csoronellas@emagister.com>
2012-04-30 20:56:33 +0200

    Test

--- Entity/EventRepository.php
+++ Entity/EventRepository.php
@@ -4,7 +4,7 @@ namespace SFBCN\EventbriteBundle\Entity;
 
 use SFBCN\EventbriteBundle\Eventbrite\AbstractRepository;
 
-cla EventRepository extends AbstractRepository
+cl EventRepository extends AbstractRepository
 {
     /**
      * Gets an event by its ID
