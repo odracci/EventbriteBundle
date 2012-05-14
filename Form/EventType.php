7576caca1dc7993a758648728aaf737e8e2a302c
Christian Soronellas <theunic@gmail.com>
2012-05-15 00:07:56 +0200

    Added EventType form class and its unit test. Updated PHPUnit bootstraping file. Updated composer files

---
+++ Form/EventType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace SFBCN\EventbriteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilder;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilder $builder, array $options)
+    {
+        $builder
+            ->add('title',              'text')
+            ->add('description',        'textarea')
+            ->add('start_date',         'datetime')
+            ->add('end_date',           'datetime')
+            ->add('timezone',           'timezone')
+            ->add('privacy',            'choice', array('choices' => array('0' => 'Private', '1' => 'Public')))
+            ->add('capacity',           'integer')
+            ->add('currency',           'text')
+            ->add('status',             'text')
+        ;
+    }
+
+    /**
+     * Returns the name of this type.
+     *
+     * @return string The name of this type
+     */
+    public function getName()
+    {
+        return 'sfbcn_eventbritebundle_eventtype';
+    }
+
+    /**
+     * Get default options for this form
+     *
+     * @return array
+     */
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class'      => 'SFBCN\EventbriteBundle\Entity\Event',
+            'csrf_protection' => true,
+            'intention'       => 'sfbcn_eventbrite_event'
+        );
+    }
+}
